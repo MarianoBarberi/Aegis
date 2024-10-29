06a90892169b886e542c56b53d70b22fb703898f
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2024-10-28 18:09:44 -0600

    clean ai output

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from OpenAI import initialize_llm, analyze_row, create_rag_chain
 from dbConnect import create_connection, check_new_rows, post_output, read_last_id_from_db, write_last_id_to_db
 from IsolationForest import cargar_y_preprocesar_logs, entrenar_isolation_forest, ejecutar_modelo
 import warnings
+import re
 
 # Configure logging
 logging.basicConfig(level=print, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -23,7 +24,7 @@ def main():
         last_IsolationForest_id = read_last_id_from_db(cursor, column="last_IsolationForest_id")
         
         while True:
-            new_rows = check_new_rows(cursor, last_Open_id, "Logs")
+            new_rows = check_new_rows(cursor, last_IsolationForest_id, "Logs")
 
             if new_rows:
                 print(f"Found {len(new_rows)} new rows:")
@@ -31,25 +32,34 @@ def main():
                 print('cargado y preprocesado')
                 model_if = entrenar_isolation_forest(X_inicial)
                 print('entrenado')
-                for row in new_rows:
-                    print(row)
-                    ejecutar_modelo(model_if, conn)
-                    print('predecido')
+                ejecutar_modelo(model_if, conn)
+                print('predecido')
+            else:
+                print("No new rows found.")
 
+            new_rows2 = check_new_rows(cursor, last_Open_id, "IsolationForest")
+            if new_rows2:
+                print(f"Found new rows:")
                 rag_chain = create_rag_chain(llm)
-                new_rows2 = check_new_rows(cursor, last_Open_id, "IsolationForest")
                 for row in new_rows2:
                     print(row)
 
                     # Analyze the row using the LLM
                     analysis_result_openai = analyze_row(llm, row, rag_chain)
+                    
                     print(analysis_result_openai["answer"])
 
+                    match = re.search(r'\{(.*?)\}', analysis_result_openai['answer'], re.DOTALL)
+                    if match:
+                        analysis_result_openai['answer'] = match.group(0)
+                    else:
+                        analysis_result_openai['answer'] = "Error: No match found."
+
                     # Post the analysis result to the database
-                    post_output(conn, analysis_result_openai["answer"], "OpenAI")
+                    post_output(conn, analysis_result_openai["answer"], row[6], row[2] ,"OpenAI")
 
                 # Update last_id to the ID of the most recent row
-                last_Open_id = new_rows[-1][0]
+                last_Open_id = new_rows2[-1][0]
                 write_last_id_to_db(cursor, conn, last_Open_id, last_IsolationForest_id)
             else:
                 print("No new rows found.")
