75c0a01ae2dfa1099e9d42f6a6cbfe6b0522f217
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2024-10-28 18:09:56 -0600

    output to db correctly

--- dbConnect.py
+++ dbConnect.py
@@ -30,16 +30,12 @@ def check_new_rows(cursor, last_id, table_name):
     cursor.execute(query, (last_id,))
     return cursor.fetchall()
 
-def post_output(conn, response, table_name):
+def post_output(conn, response, ubicacion, fecha, table_name):
     """Posts the analysis result to the database."""
     try:
         cursor = conn.cursor()
-        query = f"""
-        INSERT INTO {table_name} (description)
-        VALUES (%s)
-        """
-        data = (str(response),)
-        cursor.execute(query, data)
+        query = f"INSERT INTO {table_name} (descripcion, ubicacion, fecha) VALUES (%s, %s, %s)"
+        cursor.execute(query, (str(response), ubicacion, fecha))
         conn.commit()
         cursor.close()
         logging.info("Output successfully posted to the database.")
@@ -55,10 +51,14 @@ def read_last_id_from_db(cursor, column):
         return result[0]
     return 0 
 
-def write_last_id_to_db(cursor, conn, last_id):
+def write_last_id_to_db(cursor, conn, last_id, last_IsolationForest_id):
     """Writes the last processed ID to the database."""
-    query = "INSERT INTO LastId (last_processed_id) VALUES (%s)"
-    cursor.execute(query, (last_id,))
+    # Convert numpy.int64 to Python int
+    last_id = int(last_id)
+    last_IsolationForest_id = int(last_IsolationForest_id)
+    
+    query = "INSERT INTO LastId (last_processed_id, last_IsolationForest_id) VALUES (%s, %s)"
+    cursor.execute(query, (last_id, last_IsolationForest_id))
     conn.commit()
 
 def read_all_from_table(conn, cursor, table_name):
